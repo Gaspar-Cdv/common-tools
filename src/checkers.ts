fb390b858bf06519e5d9e55bd4be0542dca04163
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-05-30 19:56:31 +0200

    refactor: checkers

--- src/checkers.ts
+++ src/checkers.ts
@@ -3,7 +3,7 @@
  * The strict argument defines if the number must be strictly positive.
  * Default is true.
  */
-export function checkPositiveInteger (number: number, name: string, strict = true): void {
+export function checkPositiveInteger (number: number, name = 'given', strict = true): void {
   const isNegative = strict ? number <= 0 : number < 0
   if (isNegative || !Number.isInteger(number)) {
     throw TypeError(`The ${name} argument must be a ${strict ? 'strictly ' : ''}positive integer.`)
