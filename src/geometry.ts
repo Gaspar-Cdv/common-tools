65133e3f09186b0a8631ea5fef47547051291453
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-05-30 16:18:07 +0200

    fix: geometry

--- src/geometry.ts
+++ src/geometry.ts
@@ -3,7 +3,7 @@
  * If manhattan is true, the distance is calculated as the sum of the absolute differences of the x and y coordinates.
  * Otherwise, euclidean distance is used.
  */
-function distance (x1: number, y1: number, x2: number, y2: number, manhattan = false): number {
+export function distance (x1: number, y1: number, x2: number, y2: number, manhattan = false): number {
   if (manhattan) {
     return Math.abs(x1 - x2) + Math.abs(y1 - y2)
   }
@@ -16,7 +16,7 @@ function distance (x1: number, y1: number, x2: number, y2: number, manhattan = f
 /**
  * Calculate the coordinates of the middle of two points.
  */
-function middle (x1: number, y1: number, x2: number, y2: number): [number, number] {
+export function middle (x1: number, y1: number, x2: number, y2: number): [number, number] {
   return [(x1 + x2) / 2, (y1 + y2) / 2]
 }
 
@@ -24,23 +24,32 @@ function middle (x1: number, y1: number, x2: number, y2: number): [number, numbe
 /**
  * Calculate the slope of the line going through two points.
  */
-function slope (x1: number, y1: number, x2: number, y2: number): number {
-  return (y2 - y1) / (x2 - x1)
+export function slope (x1: number, y1: number, x2: number, y2: number): number {
+  if (x1 === x2) {
+    return Infinity
+  }
+  const result = (y2 - y1) / (x2 - x1)
+  return result === -0 ? 0 : result
 }
 
 
 /**
  * Calculate the y-intercept of the line going through two points.
  */
-function yIntercept (x1: number, y1: number, x2: number, y2: number): number {
-  return (x2 * y1 + x1 * y2) / (x2 - x1) // or y1 - slope(x1, y1, x2, y2) * x1
+export function yIntercept (x1: number, y1: number, x2: number, y2: number): number {
+  const lineSlope = slope(x1, y1, x2, y2)
+  if (lineSlope === Infinity || (x1 === x2 && y1 === y2)) {
+    return Infinity
+  }
+  const result = y1 - lineSlope * x1
+  return result === -0 ? 0 : result
 }
 
 
 /**
  * Calculate the equation of the line going through two points.
  */
-function equation (x1: number, y1: number, x2: number, y2: number): [number, number] {
+export function equation (x1: number, y1: number, x2: number, y2: number): [number, number] {
   let a = (y2 - y1) / (x2 - x1)
   let b = y1 - (x1 * a)
   return [a, b]
@@ -50,7 +59,7 @@ function equation (x1: number, y1: number, x2: number, y2: number): [number, num
 /**
  * Convert a slope and y-intercept to an string equation (like ax + b).
  */
-function linearToString (a: number, b: number): string {
+export function linearToString (a: number, b: number): string {
   const A = a == 0 ? '' : a == 1 ? 'x' : a == -1 ? '-x' : a + 'x'
   const B = b == 0 ? '' : A == '' ? b : (b > 0 ? ' + ' : ' - ') + Math.abs(b)
   return A + B
@@ -60,7 +69,7 @@ function linearToString (a: number, b: number): string {
 /**
  * Calculate the equation of the line of ax + b going through a point.
  */
-function perpendicular (a: number, b: number, x: number, y: number): [number, number] {
+export function perpendicular (a: number, b: number, x: number, y: number): [number, number] {
   a = -1 / a
   b = y - (x * a)
   return [a, b]
@@ -70,7 +79,7 @@ function perpendicular (a: number, b: number, x: number, y: number): [number, nu
 /**
  * Calculate the coordinates of the points intersecting a circule of center (x, y) and radius r, with a line of equation (a, b).
  */
-function circleIntersection (a: number, b: number, x: number, y: number, r: number): [number, number][] {
+export function circleIntersection (a: number, b: number, x: number, y: number, r: number): [number, number][] {
   // explanations :
   // Y = aX + b
   // (X-x)^2 + (Y-y)^2 == r^2
@@ -88,7 +97,7 @@ function circleIntersection (a: number, b: number, x: number, y: number, r: numb
 /**
  * Calculate the squarest rectangle of the specified area.
  */
-function squarest (area: number): [number, number] {
+export function squarest (area: number): [number, number] {
   for (let w = Math.ceil(area ** 0.5); w > 0; w--) {
     let min = Math.floor(area / w)
     let max = Math.ceil(area / w)
