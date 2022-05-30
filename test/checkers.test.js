f2dc0e12452860ed78d80f6231fd59939c8f5a47
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-05-30 19:56:21 +0200

    test: checkers

---
+++ test/checkers.test.js
@@ -0,0 +1,16 @@
+const { checkPositiveInteger } = require('../generated/src/checkers')
+
+describe('checkPositiveInteger', () => {
+  it('should throw an error if the parameter is not a strictly positive integer', () => {
+    expect(() => checkPositiveInteger(0)).toThrow()
+    expect(() => checkPositiveInteger(-1)).toThrow()
+    expect(() => checkPositiveInteger(1.5)).toThrow()
+    expect(() => checkPositiveInteger(1)).not.toThrow()
+  })
+  it('should throw an error if the parameter is not a positive integer', () => {
+    expect(() => checkPositiveInteger(0, '', false)).not.toThrow()
+    expect(() => checkPositiveInteger(-1, '', false)).toThrow()
+    expect(() => checkPositiveInteger(1.5, '', false)).toThrow()
+    expect(() => checkPositiveInteger(1, '', false)).not.toThrow()
+  })
+})
