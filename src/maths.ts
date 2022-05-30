32012cbc5a0fc5efb4052f795d1394d2f6bce67a
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-05-30 17:38:10 +0200

    fix: maths

--- src/maths.ts
+++ src/maths.ts
@@ -13,6 +13,11 @@ export function nthRoot (number: number, n: number): number {
  * This is the equivalent of Σ.
  */
 export function sigma (callback: (x: number) => number, a: number, b: number): number {
+  checkPositiveInteger(a, 'a')
+  checkPositiveInteger(b, 'b', false)
+  if (a > b) {
+    throw RangeError('The a argument must be less than or equal to the b argument.')
+  }
   return [...Array(b - a)].map((_, i) => callback(i + a)).reduce((a, b) => a + b, 0)
 }
 
@@ -22,7 +27,10 @@ export function sigma (callback: (x: number) => number, a: number, b: number): n
  * If the precision is positive, the result is rounded to the right of the decimal point.
  * If the precision is negative, the result is rounded to the left of the decimal point.
  */
-export function round (number: number, precision: number): number {
+export function round (number: number, precision = 0): number {
+  if (!Number.isInteger(precision)) {
+    throw TypeError('The precision argument must be an integer.')
+  }
   return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision)
 }
 
@@ -31,6 +39,9 @@ export function round (number: number, precision: number): number {
  * Clamp a number between a min and a max.
  */
 export function clamp (number: number, min: number, max: number): number {
+  if (max < min) {
+    [min, max] = [max, min]
+  }
   return Math.min(Math.max(number, min), max)
 }
 
@@ -39,7 +50,7 @@ export function clamp (number: number, min: number, max: number): number {
  * It works with negative numbers.
  */
 export function modulo (a: number, n: number): number {
-  return ((a % n) + n) % n
+  return ((a % n) + n) % n || 0
 }
 
 
@@ -47,8 +58,8 @@ export function modulo (a: number, n: number): number {
  * Determine if the number is prime
  */
 export function isPrime (n: number): boolean {
-  if (n < 2 || n % 2 === 0) return false
   if (n === 2) return true
+  if (n < 2 || n % 2 === 0) return false
   for (let i = 3; i <= Math.sqrt(n); i += 2) {
     if (n % i === 0) return false
   }
@@ -60,10 +71,11 @@ export function isPrime (n: number): boolean {
  * Fill a number with non-significant leading zeros.
  */
 export function fillZeros (number: number, length: number): string {
+  checkPositiveInteger(length, 'length')
   const sign = number >= 0 ? '' : '-'
   const stringNumber = Math.abs(number).toString().split('.')
   stringNumber[0] = stringNumber[0].padStart(length, '0')
-  return sign + stringNumber.join('')
+  return sign + stringNumber.join('.')
 }
 
 
@@ -72,14 +84,16 @@ export function fillZeros (number: number, length: number): string {
  * If the step is negative, the array will be reversed.
  */
 export function range (a: number, b = 0, step = 1): number[] {
-  if (a > b) [a, b] = [b, a];
+  if (a > b) {
+    [a, b] = [b, a]
+  }
   const array = []
   if (step > 0) {
     for (let i = a; i < b; i += step) {
       array.push(i)
     }
   } else {
-    for (let i = a; i > b; i += step) {
+    for (let i = b; i > a; i += step) {
       array.push(i)
     }
   }
@@ -91,7 +105,7 @@ export function range (a: number, b = 0, step = 1): number[] {
  * Calculate recursively the factorial of a number.
  */
 export function factorial (n: number): number {
-  return n == 1 ? n : n * factorial(n - 1)
+  return n < 2 ? 1 : n * factorial(n - 1)
 }
 
 
